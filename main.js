a787139c16bd884a1645f7cef900d83e2c33deb0
sunkanmi <oguntimehins@gmail.com>
2021-01-15 12:57:56 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const btns = document.querySelectorAll('.about-btn');
+const about = document.querySelector('.about');
+const contents = document.querySelectorAll('.content');
+
+about.addEventListener('click', (e) =>{
+  const id = e.target.dataset.id;
+  if(id){
+    btns.forEach((btn) =>{
+      btn.classList.remove('active');
+      e.target.classList.add('active');
+    });
+    contents.forEach((content) =>{
+      content.classList.remove('active');
+    })
+    const ele = document.getElementById(id)
+    ele.classList.add(active)
+  }
+})
